429269fa5a1cd1a763f69d783789e05c0a94d0aa
rcastillo9414 <rcastillom@ucenfotec.ac.cr>
2025-11-20 20:16:42 -0600

    FEAT
    Se crea un middleware que revisa si el usuario tiene un token válido (JWT) antes de permitirle acceder a ciertas rutas protegidas en el sistema.
    
    Verifica si la petición incluye un token de seguridad (JWT) en la cabecera o en la URL, y si el token es válido. Si todo está correcto, permite continuar; si no, bloquea el accesso a la pagina

---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+// Trae la libreria que ayuda a crear y verificar los token de seguridad 
+const jwt = require('jsonwebtoken'); 
+
+// Revisa el modelo de usuario para buscar informacion en la base de datos 
+const User = require('../models/User');
+
+// Se utiliza para la verificacion si un usuario tiene permiso para ingresar 
+module.exports = async (req, res, next) => {
+    //Revisa el token para aprobacion 
+    const token = req.header('Autorizacion')?.replace('Bearer', '') || req.query.token; 
+    // Si no hay token, rechaza la solicitud y muestra el mensaje
+    if (!token) return res.status(401).json({ msg: 'Acceso Restringido, revise sus credenciales'});
+
+    try { 
+        // Verificacion del token usando clave secreta
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'devsecret');
+        // Guarda la informacion del usuario que viene en el token 
+        req.user = decoded; 
+
+        // Revisa la base de datos por mas detalles (sin contraseña)
+        req.userDetails = await User.findById(decoded.id).select('-password');
+        
+        // Continua con el siguiente proceso
+        next();
+    } catch (err) { 
+        // Si el token no se verifica, muestra el siguiente mensaje 
+        console.error('Error de verificacion:', err.message);
+        res.status(401).json({ msg: 'Acceso restringido, revise sus credenciales'});
+    }
+}
